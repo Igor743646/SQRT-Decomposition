6e46a4e3e59b4f8187c4adf5f0df26aeb2e0b9af
Igor743646 <71284671+Igor743646@users.noreply.github.com>
2022-04-12 12:55:42 +0300

    Add files via upload

---
+++ SQRTDecomposition.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <vector>
+#include <functional>
+#include <cmath>
+
+template<class T>
+class SQRTDecomposition {
+
+    std::vector<T> data;
+    std::vector<T> blocks;
+    size_t size;
+    std::function<T(const T&, const T&)> func;
+
+public:
+
+    SQRTDecomposition(const std::vector<T>& v, std::function<T(const T&, const T&)> _func) {
+        data = v;
+        func = _func;
+        size = (size_t)std::sqrt(v.size());
+        size_t count_of_blocks = (v.size() % size == 0 ? v.size() / size : v.size() / size + 1);
+        blocks.resize(count_of_blocks);
+
+        for (size_t i = 0; i < count_of_blocks; i++) {
+            blocks[i] = v[size * i];
+
+            for (size_t j = 1; j < size and size * i + j < v.size(); j++) {
+                blocks[i] = func(blocks[i], v[size * i + j]);
+            }
+        }
+    }
+
+    SQRTDecomposition(const std::vector<T>& v) : SQRTDecomposition(v, [](const T& a, const T& b) {return std::max(a, b); }) { }
+
+    T Find(size_t i, size_t j) {
+        T result = data[i];
+
+        size_t t = i;
+
+        while (t <= j) {
+            if (t % size == 0 and t + size - 1 <= j) {
+                result = func(result, blocks[t / size]);
+                t += size;
+            }
+            else {
+                result = func(result, data[t]);
+                t++;
+            }
+        }
+
+        return result;
+    }
+
+};
---
+++ main.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <random>
+#include <ctime>
+#include <chrono>
+#include <algorithm>
+
+#include "Utils.h"
+#include "SQRTDecomposition.h"
+
+using namespace std;
+
+int main()
+{
+    srand(time(nullptr));
+
+    const size_t size = 42;
+
+    vector<int> v(size);
+
+    for (size_t i = 0; i < size; i++) {
+        v[i] = rand() % 99999999;
+    }
+
+    SQRTDecomposition<int> sqrt(v, [](int a, int b) {return std::min(a, b); });
+
+    print(v);
+    print(sqrt.Find(0, 41));
+
+    /*for (size_t size = 1000; size < 500000; size += 1000) {
+        vector<int> v(size);
+
+        for (size_t i = 0; i < size; i++) {
+            v[i] = rand() % 99999999;
+        }
+
+        SQRTDecomposition<int> sqrt(v);
+
+        chrono::time_point<chrono::system_clock>  start1 = chrono::system_clock::now();
+        for (size_t i = 0; i < 800; i++) {
+            sqrt.Find(0, size - 1);
+        }
+        chrono::time_point<chrono::system_clock>  end1 = chrono::system_clock::now();
+        chrono::time_point<chrono::system_clock>  start2 = chrono::system_clock::now();
+        for (size_t i = 0; i < 800; i++) {
+            size_t a = rand() % size, b = rand() % size;
+            if (a > b) std::swap(a, b);
+            sqrt.Find(a, b);
+        }
+        chrono::time_point<chrono::system_clock>  end2 = chrono::system_clock::now();
+        cout << size << " " << chrono::duration<double>(end1 - start1).count() + (chrono::duration<double>(end2 - start2).count() / 800) << "\n";
+    }*/
+}
